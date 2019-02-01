bb456f773eb1b94c28ce0d300d21104acd27b3b0
jl-randazzo <jl_randazzo@hotmail.com>
2019-02-01 09:03:09 -0700

    created PopNext() function. It is working properly.

--- MALAnalyzer/MalLine.cpp
+++ MALAnalyzer/MalLine.cpp
@@ -1,10 +1,12 @@
 #include "stdafx.h"
 #include "MalLine.h"
 #include <string>
+#include <iostream>
 #include <regex>
 
 
 using namespace std;
+
 
 
 MalLine::MalLine(string line) : _line(line) { ProcessLine(); }
@@ -47,18 +49,48 @@ string MalLine::GetErrorMessage()
 void MalLine::ProcessLine()
 {
 	int start = _line.find_first_not_of(" ", 0);
-	if (start >= 0)
+	_commentIndex = _line.find_first_of(";", 0);
+	if (start >= 0 && (_commentIndex == -1 || start < _commentIndex))
 	{
+		_emptyLine = false;
 		int len = _line.length() - start;
-		string targ = _line.substr(start, len);
+		_lineNoComment = _line.substr(start, len);
+		string workingString = _lineNoComment;
+		string targ = PopNext(workingString);
 		if (regex_match(targ, labelRegex))
 		{
-			_commentIndex = start;
-			_emptyLine = false;
+			cout << "label";
 		}
 		else if (regex_match(targ, addRegex))
 		{
-			_emptyLine = false;
+			cout << "add";
 		}
 	}
+	else
+	{
+		_emptyLine = true;
+	}
+}
+
+//adjustments to the passed in string are made purposefully
+string MalLine::PopNext(string &line)
+{
+	int term = line.find_first_of(" ", 0);
+	if (term == -1)
+	{
+		string ret = line;
+		line = "";
+		return ret;
+	}
+	else
+	{
+		string ret = line.substr(0, term);
+		line = line.substr(term + 1, line.length() - (term + 1));
+		return ret;
+	}
+}
+
+void MalLine::ValidateNext(string &next, WordType type, bool finalOp)
+{
+	return;
 }
--- MALAnalyzer/MalLine.h
+++ MALAnalyzer/MalLine.h
@@ -5,6 +5,12 @@
 static const std::regex labelRegex = std::regex("^([a-zA-Z]{1,5}):$");
 static const std::regex addRegex = std::regex("^ADD$");
 
+
+enum WordType
+{
+	Register, Immediate, MemAddress, LabelPointer
+};
+
 class MalLine {
 public:
 	//constructor and ostream operator
@@ -19,15 +25,12 @@ public:
 private:
 	//private variables
 	std::string _line;
+	std::string _lineNoComment;
 	bool _emptyLine;
 	int _commentIndex;
 	std::string _errorMessage;
 	//line processor
 	void ProcessLine();
+	std::string PopNext(std::string &line);
 	void ValidateNext(std::string &next, WordType type, bool finalOp);
-};
-
-enum WordType
-{
-	Register, Immediate, MemAddress, LabelPointer
 };
