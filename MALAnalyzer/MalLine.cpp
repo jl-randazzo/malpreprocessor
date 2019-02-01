e52258b8f44df6f53c5118a62c104093bc6930be
jl-randazzo <jl_randazzo@hotmail.com>
2019-02-01 09:59:56 -0700

    adjustments to the naming semantics were made. 'WorkingCopy' string

--- MALAnalyzer/MalLine.cpp
+++ MALAnalyzer/MalLine.cpp
@@ -7,8 +7,6 @@
 
 using namespace std;
 
-
-
 MalLine::MalLine(string line) : _line(line) { ProcessLine(); }
 
 ostream & operator <<(ostream &out, const MalLine &malLine)
@@ -60,8 +58,8 @@ void MalLine::ProcessLine()
 		_emptyLine = false;
 		int len = _line.length() - start;
 		_lineNoComment = _line.substr(start, len);
-		string workingString = _lineNoComment;
-		string targ = PopNext(workingString);
+		string workingCopy = _lineNoComment;
+		string targ = PopNext(workingCopy);
 
 		if (regex_match(targ, labelRegex)) //Is this a label?
 		{
@@ -70,7 +68,7 @@ void MalLine::ProcessLine()
 		else if (regex_match(targ, addRegex)) //Is this an Add instruction?
 		{
 			cout << "add";
-			string arg1 = PopNext(workingString), arg2 = PopNext(workingString), arg3 = PopNext(workingString);
+			string arg1 = PopNext(workingCopy), arg2 = PopNext(workingCopy), arg3 = PopNext(workingCopy);
 			_validLine = ValidateWord(arg1, Register, false) &&
 				ValidateWord(arg2, Register, false) &&
 				ValidateWord(arg3, Register, true);
@@ -82,20 +80,20 @@ void MalLine::ProcessLine()
 	}
 }
 
-//adjustments to the passed in string are made purposefully
-const string MalLine::PopNext(string &line) const
+//adjustments to the workingCopy string are made purposefully
+const string MalLine::PopNext(string &workingCopy) const
 {
-	int term = line.find_first_of(" ", 0);
+	int term = workingCopy.find_first_of(" ", 0);
 	if (term == -1)
 	{
-		string ret = line;
-		line = "";
+		string ret = workingCopy;
+		workingCopy = "";
 		return ret;
 	}
 	else
 	{
-		string ret = line.substr(0, term);
-		line = line.substr(term + 1, line.length() - (term + 1));
+		string ret = workingCopy.substr(0, term);
+		workingCopy = workingCopy.substr(term + 1, workingCopy.length() - (term + 1));
 		return ret;
 	}
 }
